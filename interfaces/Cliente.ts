db8d5264b8d7973544853c8d6627d3f8472dd26d
Fernanda Raiza <nandaraiza@hotmail.com>
2024-07-02 20:20:29 -0300

    exercicio semana 3

--- interfaces/Cliente.ts
+++ interfaces/Cliente.ts
@@ -1,9 +1,13 @@
+
+import { Gerente } from "../classes/Gerente";
+
 interface Cliente {
     nome: string;
     id: string;
     endereco: string;
     telefone: string;
     rendaSalarial: number;
+    gerente: Gerente;
 }
 
 export class Clientes implements Cliente {
@@ -12,6 +16,9 @@ export class Clientes implements Cliente {
         public id: string,
         public endereco: string,
         public telefone: string,
-        public rendaSalarial: number
+        public rendaSalarial: number,
+        gerente: Gerente
     ) {}
+    gerente: Gerente;
 }
+
